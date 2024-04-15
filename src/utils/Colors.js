4c1046868665994a7e64467203b38d526d8a164f
Febin Mathew <febnmathew98@gmail.com>
2024-04-16 01:01:13 +0530

    color name changed to cowdung

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -5,11 +5,11 @@
 export const colorTheme = [
     {id:1,name:'Default Blue', primary: '#2196f3', background: '#000814' },
     {id:2,name:'Violet', primary: '#7b2cbf', background: '#10002b' },
-    {id:3,name:'Purple', primary: '#6a4c93', background: '#000814' },
+    {id:3,name:'Grape', primary: '#6a4c93', background: '#000814' },
     {id:4,name:'Pink', primary: '#ff0054', background: '#000814' },
     {id:5,name:'Brown', primary: '#7f4f24', background: '#000814' },
     {id:6,name:'Orange', primary: '#ff5400', background: '#000814' },
-    {id:7,name:'Green', primary: '#245501', background: '#000814' },
+    {id:7,name:'Cow Dung', primary: '#245501', background: '#000814' },
 
     
 ]
